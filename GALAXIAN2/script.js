af729583b0d363f26f68e7ee8c4d79b86aa1b83b
Dominic Myers <annoyingmouse@gmail.com>
2019-03-30 12:41:57 +0000

    Prettier with obj spread

---
+++ GALAXIAN2/script.js
@@ -0,0 +1,18 @@
+import Ship from "./Ship.js";
+
+new p5(p5 => {
+
+    const width = 400;
+    const height = 800;
+    const wobble_speed = 2;
+    const wobble_amount = 100;
+
+    p5.setup = () => {
+
+    };
+
+    p5.draw = () => {
+    };
+
+});
+
--- TAIL6/script.js
+++ TAIL6/script.js
@@ -1,41 +1,31 @@
-// Constants that control the wobble effect
-const segment_size = 50;  // pixels from one segment to the next
-const angle = 2.5;        // Base direction for the tail (radians)
-const phase_step = 0.3;   // How much the phase differs in each tail piece (radians)
-const wobble_amount = 0.8;// How much of a wobble there is (radians)
-const speed = 4.0;        // How fast the wobble moves (radians per second)
-const width = 600;
-const height = 600;
-let seconds = 0;
-let tail_pieces = [];
+import TailPeice from "./TailPiece.js";
 
-function setup() {
-    createCanvas(width, height);
-    tail_pieces = Array.from({
-        length: 10
-    }, (_, i) => ({
-        img: loadImage('images/tail_piece.png'),
-        x: null,
-        y: null
-    }));
-    tail_pieces.push({
-        img: loadImage('images/tail_hook.png'),
-        x: null,
-        y: null
-    });
-}
-function draw() {
-    clear();
-    seconds += 0.01;
-    let x = width - (segment_size * 2);
-    let y = height - (segment_size * 2);
-    tail_pieces.forEach(function (p, index, arr) {
-        p.x = x;
-        p.y = y;
-        const temp_angle = angle + wobble_amount * Math.sin(index * phase_step + seconds * speed);
-        x += segment_size * Math.cos(temp_angle)
-        y -= segment_size * Math.sin(temp_angle)
-    });
-    tail_pieces.filter((p, index) => index % 2 === 0 && image(p.img, p.x, p.y));
-    tail_pieces.filter((p, index) => index % 2 === 1 && image(p.img, p.x, p.y));
-}
+new p5(p5 => {
+
+    const tail_pieces = [];
+    const width = 600;
+    const height = 600;
+    const size = 50;
+    let timer = 0;
+
+    p5.setup = () => {
+        p5.createCanvas(width, height);
+        tail_pieces.push(...Array.apply(null, Array(9)).map(() => new TailPeice(p5, null, null, 'images/tail_piece.png')));
+        tail_pieces.push(new TailPeice(p5, null, null, 'images/tail_hook.png'));
+    };
+
+    p5.draw = () => {
+        p5.background(0);
+        timer += 0.01;
+        let coordinates = {
+            x: width - (size * 2),
+            y: height - (size * 2)
+        };
+        tail_pieces.forEach(function (p, index) {
+            coordinates = { ...p.update({ ...coordinates, index, timer }) };
+        });
+        tail_pieces.filter((p, index) => index % 2 === 0 && p.draw());
+        tail_pieces.filter((p, index) => index % 2 === 1 && p.draw());
+    };
+
+});
