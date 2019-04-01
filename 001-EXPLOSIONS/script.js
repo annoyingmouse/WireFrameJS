34ac3129e1af97dde28d3d97f54e0525c1ff1f9e
Dominic Myers <annoyingmouse@gmail.com>
2019-04-01 13:03:21 +0100

    Smaller change

--- 001-EXPLOSIONS/script.js
+++ 001-EXPLOSIONS/script.js
@@ -6,7 +6,7 @@ new p5(p5 => {
     const particles = [];
     const framerate = 10;
     const speed = 60;
-    
+
     p5.setup = () => {
         p5.createCanvas(width, height);
         p5.stroke(255, 255, 255);
@@ -21,9 +21,10 @@ new p5(p5 => {
         if (p5.frameCount % framerate === 0) {
             explode();
         }
+        var date = new Date().getSeconds();
         particles.forEach((p, index, arr) => {
             if (p.age <= 2) {
-                const age = new Date().getSeconds() - p.birth.getSeconds();
+                const age = date - p.birth;
                 p5.point(p.x, p.y);
                 p.x += p.vx * p.age;
                 p.y += p.vy * p.age;
@@ -40,13 +41,14 @@ new p5(p5 => {
     const explode = () => {
         const x = p5.random(width);
         const y = p5.random(height);
+        const dob = new Date().getSeconds();
         particles.push(...Array.apply(null, Array(100)).map(() => ({
             x,
             y,
             vx: speed * Math.pow(p5.random(1), 0.5) * Math.sin(p5.random(2 * Math.PI)),
             vy: speed * Math.pow(p5.random(1), 0.5) * Math.cos(p5.random(2 * Math.PI)),
             age: 0,
-            birth: new Date()
+            birth: dob
         })));
     }
 });
