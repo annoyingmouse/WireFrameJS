540fe7a8c421e40da764b1e68068207f14bfcbe2
Dominic Myers <annoyingmouse@gmail.com>
2019-04-03 09:28:48 +0100

    Starting off

---
+++ 008-MISSILE/script.js
@@ -0,0 +1,51 @@
+new p5(p5 => {
+
+    const width = 800;
+    const height = 400;
+    const gravity = 5;
+    const trail_length = 800;
+    const trail_brightness = 100;
+    const flare_colour = (255, 220, 160);
+    const missiles = [];
+
+    p5.setup = () => {
+        p5.createCanvas(width, height);
+        p5.stroke(255, 255, 255);
+        p5.frameRate(framerate);
+        p5.background(0);
+        explode();
+    };
+
+    p5.draw = () => {
+        p5.clear();
+        p5.background(0);
+        if (p5.frameCount % framerate === 0) {
+            explode();
+        }
+        particles.forEach((p, index, arr) => {
+            p.age += 1;
+            if (p.age <= 13) {
+                p5.point(p.x, p.y);
+                p.x += p.vx * p.age;
+                p.y += p.vy * p.age;
+                p.vx *= Math.pow(drag, p.age);
+                p.vy *= Math.pow(drag, p.age);
+            } else {
+                arr.splice(index, 1);
+            }
+        });
+
+    };
+
+    const explode = () => {
+        const x = p5.random(width);
+        const y = p5.random(height);
+        particles.push(...Array.apply(null, Array(100)).map(() => ({
+            x,
+            y,
+            vx: speed * Math.pow(p5.random(1), 0.5) * Math.sin(p5.random(2 * Math.PI)),
+            vy: speed * Math.pow(p5.random(1), 0.5) * Math.cos(p5.random(2 * Math.PI)),
+            age: 0
+        })));
+    }
+});
