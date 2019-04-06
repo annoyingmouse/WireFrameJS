318055724a4587fcad3e81c3ed7643cf1715d103
Dominic Myers <annoyingmouse@gmail.com>
2019-04-06 05:53:39 +0100

    All done now

--- 009-INVADERS/Shot.js
+++ 009-INVADERS/Shot.js
@@ -1,32 +1,67 @@
-export default class Shot{
-    constructor(p5, x){
-        this.p5 = p5,
-        this.x = x,
-        this.y = -70,
+export default class Shot {
+    constructor(p5, x) {
+        this.p5 = p5;
+        this.x = x;
+        this.y = -70;
         this.matrix = [
-            [0,1,0],
-            [1,0,0],
-            [0,1,0],
-            [0,0,1],
-            [0,1,0],
-            [1,0,0],
-            [0,1,0],
-            [0,0,1]
-        ]
-        this.width = this.matrix[0].length * 10,
-        this.height = this.matrix.length * 10
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0]
+        ];
+        this.width = this.matrix[0].length * 10;
+        this.height = this.matrix.length * 10;
+        this.impacts = 0;
+        this.exploded = false;
     }
 
-    mutableRotateLeft = () => this.matrix.push(this.matrix.shift())
-    
-    mutableRotateRight = () => this.matrix.unshift(this.matrix.pop())
+    mutableRotateLeft = () => this.matrix.push(this.matrix.shift());
+
+    mutableRotateRight = () => this.matrix.unshift(this.matrix.pop());
+
+    getShrapnel() {
+        const shapnel = [];
+        this.matrix.forEach((row, rowIndex) => {
+            row.forEach((column, columnIndex) => {
+                if (column) {
+                    shapnel.push({
+                        x: this.x + (columnIndex * 10),
+                        y: this.y + (rowIndex * 10)
+                    });
+                    this.p5.rect(this.x + (columnIndex * 10), this.y + (rowIndex * 10), 10, 10);
+                }
+            });
+        });
+        return shapnel;
+    }
+
+    impact(num) {
+        this.impacts += num;
+        return this.impacts;
+    }
+
+    explode() {
+        this.matrix[0] = [0, 0, 1, 0, 0, 0];
+        this.matrix[1] = [1, 0, 0, 0, 1, 0];
+        this.matrix[2] = [0, 0, 1, 1, 0, 1];
+        this.matrix[3] = [0, 1, 1, 1, 1, 0];
+        this.matrix[4] = [1, 0, 1, 1, 1, 0];
+        this.matrix[5] = [0, 1, 1, 1, 1, 1];
+        this.matrix[6] = [1, 0, 1, 1, 1, 0];
+        this.matrix[7] = [0, 1, 0, 1, 0, 1];
+        this.exploded = true;
+    }
 
     draw() {
         this.p5.fill(0);
-        this.p5.stroke(0);        
+        this.p5.stroke(0);
         this.matrix.forEach((row, rowIndex) => {
             row.forEach((column, columnIndex) => {
-                if(column){
+                if (column) {
                     this.p5.rect(this.x + (columnIndex * 10), this.y + (rowIndex * 10), 10, 10);
                 }
             });
--- 009-INVADERS/script.js
+++ 009-INVADERS/script.js
@@ -11,53 +11,49 @@ new p5(p5 => {
     p5.setup = () => {
         p5.createCanvas(width, height);
         p5.background(51);
-        //p5.frameRate(1);
         shields.push(...Array.from(Array(4).keys(), n => new Shield(p5, (220 * n) + (60 * (n + 1)), 500)));
-        setInterval(() => shots.push(new Shot(p5, Math.floor(p5.random(0, (((p5.width - 20) / 10)))) * 10)), 100);
+        setInterval(() => shots.push(new Shot(p5, Math.floor(p5.random(0, (((p5.width - 40) / 10)))) * 10)), 1000);
     };
 
-    const collision = (shot, shield) => {
-        return (
-            shot.x + shot.width > shield.x &&
-            shot.x < shield.x + shield.width &&
-            shot.y + shot.height > shield.y &&
-            shot.y < shield.y + shield.height
-        )
+    const drawShots = () => {
+        let index = shots.length;
+        while (index--) {
+            if (shots[index].y > p5.height) {
+                shots.splice(index, 1);
+            } else {
+                shots[index].draw();
+                if (shots[index].exploded) {
+                    shields.forEach(shield => {
+                        shield.erode(shots[index].getShrapnel())
+                    });
+                    shots.splice(index, 1);
+                }
+            }
+        }
     }
 
-    const drawShots = () => {
-        let shotIndex = shots.length;
-        while(shotIndex--){
-            if(shots[shotIndex].y > p5.height){
-                shots.splice(shotIndex, 1);
-            }else{
-                shots[shotIndex].draw();
+    const drawShields = () => {
+        let index = shields.length;
+        while (index--) {
+            if (shields[index].empty()) {
+                shields.splice(index, 1);
+            } else {
+                shields[index].draw();
             }
         }
     }
 
     p5.draw = () => {
         p5.clear()
-        p5.background(51);
-
+        p5.background(255);
         shields.forEach(shield => {
-            shots.forEach(shot => {
-                if (collision(shield, shot)) {
-                    console.log("Do something here");
+            shots.forEach((shot, shotIndex) => {
+                if (shot.impact(shield.erode(shot.getShrapnel())) > 3) {
+                    shot.explode(shotIndex);
                 }
-            })
+            });
         });
-        shields.forEach(shield => shield.draw());
         drawShots();
-
-
-        
-
-
-        
-
-
-
+        drawShields();
     };
-
 });
