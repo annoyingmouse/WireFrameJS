76192780cb859d72b825acc0241df7076813cccd
Dominic Myers <annoyingmouse@gmail.com>
2019-04-07 08:07:45 +0100

    Sort of starting position

--- 002-GALAXIAN2/Ship.js
+++ 002-GALAXIAN2/Ship.js
@@ -1,8 +1,18 @@
-class Ship {
-    constructor(x, y, sprite, rotation) {
+export default class Ship {
 
+    constructor(p5, x, y, img, angle) {
+        this.p5 = p5;
+        this.x = x;
+        this.y = y;
+        this.img = this.p5.loadImage(img);
+        this.angle = angle;
     }
 
-
+    draw() {
+        this.p5.translate(this.x, this.y);
+        this.p5.translate(0, 0);
+        this.p5.rotate(this.angle)
+        this.p5.image(this.img, 0, 0);
+    }
 
 }
--- 002-GALAXIAN2/script.js
+++ 002-GALAXIAN2/script.js
@@ -6,12 +6,19 @@ new p5(p5 => {
     const height = 800;
     const wobble_speed = 2;
     const wobble_amount = 100;
+    const epsilon = 0.001;
+    let ship = null;
 
     p5.setup = () => {
-
+        p5.createCanvas(width, height);
+        p5.imageMode(p5.CENTER);
+        p5.angleMode(p5.DEGREES);
+        ship = new Ship(p5, 100, 100, 'images/ship.png', 90);
     };
 
     p5.draw = () => {
+        p5.background(0);
+        ship.draw();
     };
 
 });
