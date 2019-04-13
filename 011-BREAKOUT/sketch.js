f48d7eeedf8557f0bd6e8271f8703fd6ef10d02b
Dominic Myers <annoyingmouse@gmail.com>
2019-04-13 07:28:29 +0100

    Bundles p5 and working on breakout

---
+++ 011-BREAKOUT/sketch.js
@@ -0,0 +1,63 @@
+import Brick from "./Brick.js"
+
+new p5(p5 => {
+
+    const width = 600;
+    const height = 800;
+    const bricks = [];
+    const columns = 10;
+    const margin = 50;
+    const rows = 5;
+    const brick_width = (width - 2 * margin) / columns;
+    const brick_height = 25;
+
+
+    p5.setup = () => {
+        p5.createCanvas(width, height);
+        for (let x = 0; x < columns; x++) {
+            for (let y = 0; y < rows; y++) {
+                const hue = (x + y) / columns;
+                const saturation = (y / rows) * 0.5 + 0.5;
+                const hightlight = HSV2RGB(hue, saturation * 0.7, 1);
+                const colour = HSV2RGB(hue, saturation, 0.8);
+                bricks.push(new Brick(p5, x * brick_width + margin, y * brick_height + margin, brick_width - 1, brick_height - 1, colour, hightlight))
+            }
+        }
+        console.log(bricks);
+    };
+
+    p5.draw = () => {
+        p5.background(0);
+        bricks.forEach(brick => brick.draw());
+    };
+
+
+    const HSV2RGB = (h, s, v) => {
+        let r, g, b;
+        const i = Math.floor(h * 6);
+        const f = h * 6 - i;
+        const p = v * (1 - s);
+        const q = v * (1 - f * s);
+        const t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+        }
+        return [r * 255, g * 255, b * 255];
+    }
+
+
+    // p5.keyPressed = (p5) => {
+    //     if (p5.keyCode === 40 && enemies.length) {
+    //         const newEnemies = enemies[0].destroy();
+    //         if (newEnemies.length) {
+    //             enemies.push(...newEnemies);
+    //         }
+    //         enemies.shift();
+    //     }
+    // };
+});
