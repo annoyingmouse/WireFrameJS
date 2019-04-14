3696a8e7398c7e008786911ef8b503c066483574
Dominic Myers <annoyingmouse@gmail.com>
2019-04-14 11:41:14 +0100

    FROHHER moved over too now

---
+++ 000-MISCELLANEOUS/p5FROGGER/js/modules/Lane.js
@@ -0,0 +1,53 @@
+import Rectangle from "./Rectangle.js";
+import Obstacle from "./Obstacle.js";
+
+export default class Lane extends Rectangle {
+
+    constructor(index, grid, colour, type, num, length, spacing, speed, p5Width) {
+        super(0, index * grid, p5Width, grid);
+        this.obstacles = [];
+        this.type = type;
+        const offset = Math.floor(Math.random() * 200);
+        for (let i = 0; i < num; i++) {
+            this.obstacles.push(
+                new Obstacle(
+                    offset + spacing * i,
+                    index * grid,
+                    grid * length,
+                    grid,
+                    speed,
+                    grid,
+                    colour
+                )
+            );
+        }
+    }
+
+    check(frog, p5) {
+        if (this.type === "CAR") {
+            this.obstacles.forEach(car => {
+                if (frog.intersects(car)) {
+                    p5.resetGame();
+                }
+            });
+        } else {
+            let ok = false;
+            this.obstacles.forEach(log => {
+                if (frog.intersects(log)) {
+                    ok = true;
+                    frog.attach(log);
+                }
+            });
+            if (!ok) {
+                p5.resetGame();
+            }
+        }
+    }
+
+    run(p5) {
+        this.obstacles.forEach(obstacle => {
+            obstacle.show(p5);
+            obstacle.update(p5);
+        });
+    }
+}
---
+++ 000-MISCELLANEOUS/p5FROGGER/js/modules/Safety.js
@@ -0,0 +1,14 @@
+import Rectangle from "./Rectangle.js";
+
+export default class Safety extends Rectangle {
+
+    constructor(index, grid, p5Width) {
+        super(0, index * grid, p5Width, grid);
+        this.type = "SAFETY";
+    }
+
+    run(p5) {
+        p5.fill(100);
+        p5.rect(this.x, this.y, this.width, this.height);
+    }
+}
