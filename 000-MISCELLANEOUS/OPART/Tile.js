52ba3c60e25f664d0d69ad992a77efcde9eb79c7
Dominic Myers <annoyingmouse@gmail.com>
2019-05-03 20:04:19 +0100

    still getting there

--- 000-MISCELLANEOUS/OPART/Tile.js
+++ 000-MISCELLANEOUS/OPART/Tile.js
@@ -7,21 +7,21 @@ export default class Tile {
         this.dimension = dimension;
         this.row = row;
         this.column = column;
+        this.onFirst = true;
         this.on = p5.color(255, 184, 0);
         this.off = p5.color(26, 17, 16);
-        this.radius = Math.sqrt(Math.pow(dimension, 2) * 2)
-        this.onFirst = true;
-        this.pg = this.p5.createGraphics(dimension, dimension)
+        this.diameter = Math.sqrt(Math.pow(dimension, 2) * 2)
+        this.pg = p5.createGraphics(dimension, dimension)
         this.pg.noStroke();
     }
 
-    update(){
-        if(this.radius < 0){
-            this.radius = Math.sqrt(Math.pow(this.dimension, 2) * 2);
+    update() {
+        if (this.diameter < 0) {
+            this.diameter = Math.sqrt(Math.pow(this.dimension, 2) * 2);
             this.onFirst = !this.onFirst
         }
-        else{
-            this.radius -= 1;
+        else {
+            this.diameter -= 1;
         }
         return this.draw();
     }
@@ -30,13 +30,7 @@ export default class Tile {
         this.pg.fill(this.onFirst ? this.off : this.on);
         this.pg.rect(this.x, this.y, this.dimension, this.dimension);
         this.pg.fill(this.onFirst ? this.on : this.off);
-        this.pg.circle(this.x + this.dimension / 2, this.y + this.dimension / 2, this.radius);
-        return {
-            pg: this.pg,
-            x: this.x,
-            y: this.y
-        }
-        //context.image(this.pg, this.x, this.y, this.dimension, this.dimension)
+        this.pg.circle(this.x + this.dimension / 2, this.y + this.dimension / 2, this.diameter);
+        return this.pg;
     }
-
 }
--- 000-MISCELLANEOUS/OPART/sketch.js
+++ 000-MISCELLANEOUS/OPART/sketch.js
@@ -5,15 +5,12 @@ new p5(p5 => {
     const rows = 14;
     const columns = 14;
     const dimension = 40
-    const height = rows * dimension;
-    const width = columns * dimension;
     const framerate = 1;
     const tiles = [];
 
-
     p5.setup = () => {
-        p5.createCanvas(width, height);
-        p5.frameRate(framerate);
+        p5.createCanvas(columns * dimension, rows * dimension);
+        //p5.frameRate(framerate);
         for (let r = 0; r < rows; r++) {
             for (let c = 0; c < columns; c++) {
                 tiles.push(new Tile(p5, c * dimension, r * dimension, dimension, r, c));
@@ -24,8 +21,7 @@ new p5(p5 => {
     p5.draw = () => {
         p5.background(200);
         tiles.forEach((tile) => {
-            let pg = tile.update();
-            p5.image(pg.pg, tile.x, tile.y, dimension, dimension, 0, 0, dimension, dimension);
+            p5.image(tile.update(), tile.x, tile.y);
         });
     };
 });
