80245163fa34ce18c540b65b357bd886432f836d
Dominic Myers <annoyingmouse@gmail.com>
2019-05-03 21:55:41 +0100

    Update Tile.js

--- 000-MISCELLANEOUS/OPART/Tile.js
+++ 000-MISCELLANEOUS/OPART/Tile.js
@@ -1,27 +1,32 @@
 export default class Tile {
 
-    constructor(p5, x, y, dimension, row, column) {
+    constructor(p5, x, y, dimension, row, column, delay) {
         this.p5 = p5;
         this.x = x;
         this.y = y;
         this.dimension = dimension;
-        this.row = row;
-        this.column = column;
-        this.onFirst = true;
+        this.delay = column;
+        this.onFirst = row % 2;
         this.on = p5.color(255, 184, 0);
         this.off = p5.color(26, 17, 16);
+        this.break = delay
         this.diameter = Math.sqrt(Math.pow(dimension, 2) * 2)
         this.pg = p5.createGraphics(dimension, dimension)
         this.pg.noStroke();
     }
 
     update() {
-        if (this.diameter < 0) {
-            this.diameter = Math.sqrt(Math.pow(this.dimension, 2) * 2);
-            this.onFirst = !this.onFirst
-        }
-        else {
-            this.diameter -= 1;
+        if(this.delay === 0){
+            if (this.diameter < 0) {
+                this.diameter = Math.sqrt(Math.pow(this.dimension, 2) * 2);
+                this.onFirst = !this.onFirst;
+                this.delay = this.break;
+            }
+            else {
+                this.diameter -= 1;
+            }
+        }else{
+           this.delay -= 1;
         }
         return this.draw();
     }
