09dea4de0eee058b664dd0faa8e594fe1dafbc2a
Dominic Myers <annoyingmouse@gmail.com>
2019-05-03 07:04:12 +0100

    Getting there

--- 000-MISCELLANEOUS/OPART/Tile.js
+++ 000-MISCELLANEOUS/OPART/Tile.js
@@ -1,23 +1,38 @@
 export default class Tile {
 
-    constructor(p5, x, y, width, height, row) {
+    constructor(p5, x, y, width, height, row, column) {
         this.p5 = p5;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.row = row;
+        this.column = column;
         this.on = p5.color(255, 184, 0);
         this.off = p5.color(26, 17, 16);
-        
+        this.radiusX = width;
+        this.radiusY = height;
     }
 
     update(status) {
         this.draw()
     }
 
+    isOdd = (num) => num % 2
+
     draw() {
-        
+        let pg = this.p5.createGraphics(this.width, this.height);
+        pg.noStroke();
+        pg.fill(this.off);
+        pg.rect(this.x, this.y, this.width, this.height);
+        pg.fill(this.on);
+        pg.circle(this.x + pg.width / 2, this.y + pg.height / 2, this.radius);
+        return {
+            i: pg,
+            x: this.x,
+            y: this.y
+        };
+        this.radius -= 1;
     }
 
 }
--- 000-MISCELLANEOUS/OPART/sketch.js
+++ 000-MISCELLANEOUS/OPART/sketch.js
@@ -4,24 +4,31 @@ new p5(p5 => {
 
     const rows = 14;
     const columns = 14;
-    const dimension = 15
+    const dimension = 20
     const height = rows * dimension;
     const width = columns * dimension;
-    const framerate = 10;
+    const framerate = 1;
     const tiles = [];
-    
+
 
     p5.setup = () => {
         p5.createCanvas(width, height);
         p5.frameRate(framerate);
-        for(let r = 0; r < rows; r++){
-            for(let c = 0; c < columns; c++){
-                tiles.push(new Tile(p5, c * dimension, r * dimension, dimension, dimension, r));
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < columns; c++) {
+                //if (!r && !c) {
+                tiles.push(new Tile(p5, c * dimension, r * dimension, dimension, dimension, r, c));
+                //}
             }
         }
     };
 
     p5.draw = () => {
-        tiles.forEach((tile) => tile.update());
+        p5.background(200);
+        tiles.forEach((tile) => {
+            const img = tile.draw();
+            console.log(img);
+            p5.image(img.i, img.x, img.y);
+        });
     };
 });
