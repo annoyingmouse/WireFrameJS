414b3674d7c3bf11d9199f432b20afee387ebcb4
Dominic Myers <annoyingmouse@gmail.com>
2019-05-06 08:54:14 +0100

    Making things interactive

--- 000-MISCELLANEOUS/OPART/Tile.js
+++ 000-MISCELLANEOUS/OPART/Tile.js
@@ -1,15 +1,15 @@
 export default class Tile {
 
-    constructor(p5, x, y, dimension, row, column, initial) {
+    constructor(p5, x, y, dimension, row, column, initial, on, off) {
         this.p5 = p5;
         this.x = x;
         this.y = y;
         this.dimension = dimension;
         this.delay = column;
         this.onFirst = row % 2;
-        this.on = p5.color(255, 184, 0);
-        this.off = p5.color(26, 17, 16);
-        this.initial = initial
+        this.on = on;
+        this.off = off;
+        this.initial = initial;
         this.diameter = Math.sqrt(Math.pow(dimension, 2) * 2)
         this.pg = p5.createGraphics(dimension, dimension)
         this.pg.noStroke();
@@ -20,7 +20,7 @@ export default class Tile {
             if (this.diameter < 0) {
                 this.diameter = Math.sqrt(Math.pow(this.dimension, 2) * 2);
                 this.onFirst = !this.onFirst;
-                this.delay = this.initial + 100;
+                this.delay = this.initial;
             }
             else {
                 this.diameter -= 1;
@@ -29,6 +29,11 @@ export default class Tile {
             this.delay -= 1;
         }
         return this.draw();
+    }
+
+    setDelay(val) {
+        this.initial = val;
+        console.log(this.initial);
     }
 
     draw() {
--- 000-MISCELLANEOUS/OPART/sketch.js
+++ 000-MISCELLANEOUS/OPART/sketch.js
@@ -2,29 +2,108 @@ import Tile from "./Tile.js";
 
 new p5(p5 => {
 
-    const rows = 14;
-    const columns = 14;
-    const dimension = 40
-    const framerate = 20;
-    const tiles = [];
-    const delay = 30;
+    const style = window.getComputedStyle(document.querySelector("body"), null);
+    const bodyWidth = parseInt(style.getPropertyValue("width"), 10);;
 
-    p5.setup = () => {
-        p5.createCanvas(columns * dimension, rows * dimension);
-        //p5.frameRate(framerate);
+    const tiles = [];
+    let delay = 120;
+    let columns = 14;
+    let rows = 14;
+    let on = p5.color(255, 184, 0);
+    let off = p5.color(26, 17, 16);
+
+    let varForm, varFieldSet, delayInput, delayLabel, columnsInput, columnsLabel, onInput, onLabel, offInput, offLabel;
+
+    const reset = () => {
+        delay = delayInput.value();
+        columns = columnsInput.value();
+        rows = columnsInput.value();
+        on = onInput.value();
+        off = offInput.value();
+        tiles.length = 0;
         for (let row = 0; row < rows; row++) {
             for (let column = 0; column < columns; column++) {
                 tiles.push(new Tile(
                     p5,
-                    column * dimension,
-                    row * dimension,
-                    dimension,
+                    column * (bodyWidth / columns),
+                    row * (bodyWidth / columns),
+                    (bodyWidth / columns),
                     row,
                     column % 2 ? ((rows - row) * 5) + 80 : row * 5,
-                    20
+                    delay,
+                    on,
+                    off
                 ));
             }
         }
+    }
+
+    const createDelay = () => {
+        let delayP = p5.createP();
+        delayInput = p5.createSlider(0, 500, delay, 10);
+        delayInput.elt.setAttribute("name", "delay");
+        delayLabel = document.createElement("LABEL");
+        delayLabel.setAttribute("for", "delay");
+        delayLabel.innerText = "Delay value"
+        delayP.child(delayLabel);
+        delayP.child(delayInput);
+        varFieldSet.child(delayP);
+        delayInput.input(reset);
+    }
+
+    const createRows = () => {
+        let columnsP = p5.createP();
+        columnsInput = p5.createInput(columns.toString(), "number");
+        columnsInput.elt.setAttribute("name", "columns");
+        columnsLabel = p5.createElement("label");
+        columnsLabel.attribute("for", "columns");
+        columnsLabel.html("Number of columns/rows");
+        columnsP.child(columnsLabel);
+        columnsP.child(columnsInput);
+        varFieldSet.child(columnsP);
+        columnsInput.input(reset);
+    }
+
+    const createOn = () => {
+        let onP = p5.createP();
+        onInput = p5.createColorPicker(on);
+        onInput.elt.setAttribute("name", "on");
+        onLabel = p5.createElement("label");
+        onLabel.attribute("for", "on");
+        onLabel.html("On colour")
+        onP.child(onLabel);
+        onP.child(onInput);
+        varFieldSet.child(onP);
+        onInput.input(reset);
+    }
+
+    const createOff = () => {
+        let offP = p5.createP();
+        offInput = p5.createColorPicker(off);
+        offInput.elt.setAttribute("name", "off");
+        offLabel = p5.createElement("label");
+        offLabel.attribute("for", "off");
+        offLabel.html("Off colour");
+        offP.child(offLabel);
+        offP.child(offInput);
+        varFieldSet.child(offP);
+        offInput.input(reset);
+    }
+
+
+    p5.setup = () => {
+        p5.createCanvas(bodyWidth, bodyWidth);
+        varForm = p5.createElement("form");
+        varFieldSet = p5.createElement("fieldset");
+        const legend = p5.createElement("legend");
+        legend.html("Editable values");
+        varForm.child(varFieldSet)
+        varFieldSet.child(legend)
+        createDelay();
+        createRows();
+        createOn();
+        createOff();
+        reset();
     };
 
     p5.draw = () => {
