ffa83020c79b877e9737eae5ec3e0a7bbcdf49a1
Dominic Myers <annoyingmouse@gmail.com>
2019-05-08 16:18:57 +0100

    Working on Pendula

---
+++ 000-MISCELLANEOUS/PENDULUM/Pendulum.js
@@ -0,0 +1,36 @@
+export default class Pendulum {
+
+    constructor(p5, origin, rope_length){
+        console.log(origin);
+        this.p5 = p5;
+        this.origin = origin;
+        this.position = p5.createVector;
+        this.rope_length = rope_length;
+        this.angle = p5.PI/4;
+        this.aVelocity = 0.0;
+        this.aAcceleration = 0.0;
+        this.damping = 0.999;
+        this.ballr = 48.0;
+        this.gravity = 0.7;
+    }
+
+    update() {
+        this.aAcceleration = (-1 * this.gravity / this.rope_length) * this.p5.sin(this.angle);
+        this.aVelocity += this.aAcceleration; 
+        this.aVelocity *= this.damping;
+        this.angle += this.aVelocity;  
+        this.display();
+    }
+
+    display() {
+        this.position = this.p5.createVector(this.r * this.p5.sin(this.angle), this.r * this.p5.cos(this.angle), 0);
+        this.position.add(this.origin);
+        this.p5.stroke(255);
+        this.p5.strokeWeight(2);
+        this.p5.line(this.origin.x, this.origin.y, this.position.x, this.position.y);
+        this.p5.ellipseMode(this.p5.CENTER);
+        this.p5.fill(127);
+        console.log(this.position.y);
+        this.p5.ellipse(this.position.x, this.position.y, this.ballr, this.ballr);
+    }
+}
