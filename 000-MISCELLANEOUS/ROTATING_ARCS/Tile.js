a360efe0891a5ac30126ae9de9a53b4275ef523f
Dominic Myers <annoyingmouse@gmail.com>
2019-11-23 12:25:17 +0000

    ROTATING ARCS added

---
+++ 000-MISCELLANEOUS/ROTATING_ARCS/Tile.js
@@ -0,0 +1,39 @@
+export default class Tile {
+
+    constructor(p5, x, y, dimension, img) {
+        this.p5 = p5;
+        this.x = x;
+        this.y = y;
+        this.dimension = dimension;
+        this.img = img;
+        this.rotating = false;
+        this.degree = 0;
+        this.speed = 1;
+    }
+
+    rotate() {
+        this.rotating = true;
+        this.degree += this.speed;
+    }
+
+
+    draw() {
+        this.p5.push();
+        this.p5.translate(this.x + (this.dimension / 2), this.y + (this.dimension / 2));
+        this.p5.rotate(this.degree);
+        this.p5.translate(-this.x - (this.dimension / 2), -this.y - (this.dimension / 2));
+        this.p5.image(this.img, this.x, this.y, this.dimension, this.dimension);
+        this.p5.pop();
+        if (this.rotating) {
+            if (this.degree === 360) {
+                this.degree = 0;
+                this.rotating = false;
+            }
+            if (this.degree === 0 || this.degree === 90 || this.degree === 180 || this.degree === 270) {
+                this.rotating = false;
+            } else {
+                this.degree += this.speed;
+            }
+        }
+    }
+}
---
+++ 000-MISCELLANEOUS/ROTATING_ARCS/sketch.js
@@ -0,0 +1,33 @@
+import Tile from "./Tile.js";
+
+new p5(p5 => {
+    const style = window.getComputedStyle(document.querySelector("body"), null);
+    const bodyWidth = Math.floor(parseInt(style.getPropertyValue("width"), 10) / 10) * 10;
+    const tiles = [];
+    const dimmention = 40;
+    const spinning = 3;
+    let img;
+    p5.setup = () => {
+        p5.angleMode(p5.DEGREES);
+        p5.createCanvas(bodyWidth, bodyWidth);
+        img = p5.loadImage("assets/segment.png");
+        for (let y = 0; y < bodyWidth; y += dimmention) {
+            for (let x = 0; x < bodyWidth; x += dimmention) {
+                tiles.push(new Tile(p5, x, y, dimmention, img));
+            }
+
+        }
+    };
+    p5.draw = () => {
+        p5.background(p5.color(223, 222, 219));
+        tiles.forEach(tile => {
+            tile.draw();
+        });
+        if (tiles.filter(tile => tile.rotating).length < spinning) {
+            for (let spinners = 0; spinners < spinning; spinners++) {
+                const unRotating = tiles.filter(tile => !tile.rotating);
+                unRotating.length && unRotating[~~(Math.random() * unRotating.length)].rotate();
+            }
+        }
+    };
+});
