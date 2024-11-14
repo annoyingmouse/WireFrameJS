7a6fb048b2dce26357f30ef2679a5d41db401225
Dominic Myers <annoyingmouse@gmail.com>
2024-11-14 07:49:33 +0000

    refactor(SliderNumeral.js): optimize update method to simplify position calculations and improve code readability refactor(SliderNumeral.js): remove unused otherColumns method to clean up the codebase and reduce complexity

--- 000-MISCELLANEOUS/SLIDECLOCK/SliderNumeral.js
+++ 000-MISCELLANEOUS/SLIDECLOCK/SliderNumeral.js
@@ -31,16 +31,16 @@ export class SliderNumeral {
 
   update(num) {
     const positions = [
-      [5, 6, 10, 12], // 0
-      [0, 0, 4, 12], // 1
-      [7, 11, 3, 10], // 2
-      [9, 7, 3, 6], // 3
-      [3, 4, 8, 6], // 4
-      [11, 9, 3, 8], // 5
-      [5, 9, 3, 8], // 6
-      [1, 2, 6, 12], // 7
-      [5, 1, 3, 12], // 8
-      [11, 1, 3, 12], // 9
+      [5, 10, 12], // 0
+      [0, 4, 12], // 1
+      [7, 3, 10], // 2
+      [9, 3, 6], // 3
+      [3, 8, 6], // 4
+      [11, 3, 8], // 5
+      [5, 3, 8], // 6
+      [1, 6, 12], // 7
+      [5, 3, 12], // 8
+      [11, 3, 12], // 9
     ];
     this.pg.clear();
     if (!this.firstColumnOffset !== positions[num][0] * this.unitHeight) {
@@ -81,61 +81,42 @@ export class SliderNumeral {
           : (this.firstColumnOffset -= 1);
       }
     }
-    if (!this.secondColumnOffset !== positions[num][2] * this.unitHeight) {
-      if (this.secondColumnOffset < positions[num][2] * this.unitHeight) {
+    if (!this.secondColumnOffset !== positions[num][1] * this.unitHeight) {
+      if (this.secondColumnOffset < positions[num][1] * this.unitHeight) {
         this.secondColumnOffset = this.instant
           ? Math.abs(
-              this.secondColumnOffset - positions[num][2] * this.unitHeight,
+              this.secondColumnOffset - positions[num][1] * this.unitHeight,
             ) < 5
-            ? positions[num][2] * this.unitHeight
+            ? positions[num][1] * this.unitHeight
             : (this.secondColumnOffset += 5)
           : (this.secondColumnOffset += 1);
       } else {
         this.secondColumnOffset = this.instant
           ? Math.abs(
-              this.secondColumnOffset - positions[num][2] * this.unitHeight,
+              this.secondColumnOffset - positions[num][1] * this.unitHeight,
             ) < 5
-            ? positions[num][2] * this.unitHeight
+            ? positions[num][1] * this.unitHeight
             : (this.secondColumnOffset -= 5)
           : (this.secondColumnOffset -= 1);
       }
     }
-    if (!this.thirdColumnOffset !== positions[num][3] * this.unitHeight) {
-      if (this.thirdColumnOffset < positions[num][3] * this.unitHeight) {
+    if (!this.thirdColumnOffset !== positions[num][2] * this.unitHeight) {
+      if (this.thirdColumnOffset < positions[num][2] * this.unitHeight) {
         this.thirdColumnOffset = this.instant
           ? Math.abs(
-              this.thirdColumnOffset - positions[num][3] * this.unitHeight,
+              this.thirdColumnOffset - positions[num][2] * this.unitHeight,
             ) < 5
-            ? positions[num][3] * this.unitHeight
+            ? positions[num][2] * this.unitHeight
             : (this.thirdColumnOffset += 5)
           : (this.thirdColumnOffset += 1);
       } else {
         this.thirdColumnOffset = this.instant
           ? Math.abs(
-              this.thirdColumnOffset - positions[num][3] * this.unitHeight,
+              this.thirdColumnOffset - positions[num][2] * this.unitHeight,
             ) < 5
-            ? positions[num][3] * this.unitHeight
+            ? positions[num][2] * this.unitHeight
             : (this.thirdColumnOffset -= 5)
           : (this.thirdColumnOffset -= 1);
-      }
-    }
-    if (!this.otherColumnsOffset !== positions[num][1] * this.unitHeight) {
-      if (this.otherColumnsOffset < positions[num][1] * this.unitHeight) {
-        this.otherColumnsOffset = this.instant
-          ? Math.abs(
-              this.otherColumnsOffset - positions[num][1] * this.unitHeight,
-            ) < 5
-            ? positions[num][1] * this.unitHeight
-            : (this.otherColumnsOffset += 5)
-          : (this.otherColumnsOffset += 1);
-      } else {
-        this.otherColumnsOffset = this.instant
-          ? Math.abs(
-              this.otherColumnsOffset - positions[num][1] * this.unitHeight,
-            ) < 5
-            ? positions[num][1] * this.unitHeight
-            : (this.otherColumnsOffset -= 5)
-          : (this.otherColumnsOffset -= 1);
       }
     }
     this.draw();
@@ -149,7 +130,6 @@ export class SliderNumeral {
     this.firstColumn(0, 0, this.unitWidth, this.unitHeight);
     this.secondColumn(0, 0, this.unitWidth, this.unitHeight);
     this.thirdColumn(0, 0, this.unitWidth, this.unitHeight);
-    // this.otherColumns(0, 0, this.unitWidth, this.unitHeight);
     return this.p5.image(this.pg, this.x, this.y);
   }
 
@@ -244,45 +224,5 @@ export class SliderNumeral {
       this.black,
       true,
     );
-  };
-  otherColumns = (x, y, unitWidth, unitHeight) => {
-    const figure = [
-      [0, 1],
-      [0, 0],
-      [0, 0],
-      [0, 0],
-      [1, 0],
-      [0, 0],
-      [0, 0],
-      [0, 0],
-      [1, 0],
-      [0, 0],
-      [1, 0],
-      [0, 0],
-      [1, 0],
-    ];
-    for (let y = 0; y < figure.length; y++) {
-      for (let x = 0; x < figure[y].length; x++) {
-        if (figure[y][x] === 1) {
-          this.drawSquare(
-            (x + 1.5) * unitWidth,
-            y * unitHeight + this.otherColumnsOffset,
-            unitWidth,
-            unitHeight,
-            this.black,
-            true,
-          );
-        } else {
-          this.drawSquare(
-            (x + 1.5) * unitWidth,
-            y * unitHeight + this.otherColumnsOffset,
-            unitWidth,
-            unitHeight,
-            this.transparency,
-            true,
-          );
-        }
-      }
-    }
   };
 }
