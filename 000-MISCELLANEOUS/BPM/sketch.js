8b9a70735a86ad9f714af999be361c87e5b922cc
Dominic Myers <annoyingmouse@gmail.com>
2025-01-02 12:49:06 +0000

    DSEG14
    Should all be done now, just need to think of a use case...

--- 000-MISCELLANEOUS/BPM/sketch.js
+++ 000-MISCELLANEOUS/BPM/sketch.js
@@ -1,18 +1,95 @@
 import { p5 } from "https://cdn.skypack.dev/p5js-wrapper";
-import { Glyph } from "./Glyph.js";
+import { DSEG14 } from "./DSEG14.js";
 
 new p5((p5) => {
   let glyph;
+  let counter = 0;
+  const increment = () => {
+    counter++;
+    if (counter >= 64) {
+      counter = 0;
+    }
+    console.info(characters[counter]);
+    glyph.setState(characters[counter]);
+  };
+  const characters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    " ",
+    "?",
+    '"',
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    "/",
+    "<",
+    "=",
+    ">",
+    "@",
+    "\\",
+    "^",
+    "_",
+    "`",
+    "|",
+    "~",
+    "¥",
+    "¦",
+    "°",
+    "±",
+  ];
   p5.setup = () => {
     const canvas = p5.createCanvas(1144, 1640);
     canvas.style("display", "block");
-    canvas.style("outline", "1px solid #000");
-    // p5.frameRate(1);
-    glyph = new Glyph(p5, 20, 20, 0.25, null, null, "±");
+    canvas.style("outline", "none");
+    canvas.style("background", "transparent");
+    console.info(characters[counter]);
+    glyph = new DSEG14(p5, 0, 0, 0.1, null, null, characters[counter]);
+    setInterval(increment, 1000);
   };
 
   p5.draw = () => {
-    p5.background(255);
+    p5.background("rgba(0, 255, 0, 0)");
     glyph.draw();
   };
 });
