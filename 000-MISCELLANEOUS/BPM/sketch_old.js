b861fa91df8a786ba588673b796af4de066a282b
Dominic Myers <annoyingmouse@gmail.com>
2025-01-03 08:05:47 +0000

    🔧 (BPM): Remove duplicate code by deleting the old sketch.js file and renaming the new sketch_old.js file to sketch.js.

---
+++ 000-MISCELLANEOUS/BPM/sketch_old.js
@@ -0,0 +1,113 @@
+import { p5 } from "https://cdn.skypack.dev/p5js-wrapper";
+import { DSEG14 } from "./DSEG14.js";
+import {getTimeArray} from "./utilities.js";
+
+new p5((p5) => {
+  let glyph1;
+  let glyph2;
+  let counter = 0;
+  const increment = () => {
+    counter++;
+    if (counter >= 64) {
+      counter = 0;
+    }
+    console.info(characters[counter]);
+    glyph1.setState(characters[counter]);
+  };
+  const numerals = []
+  const characters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    " ",
+    "?",
+    '"',
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    "/",
+    "<",
+    "=",
+    ">",
+    "@",
+    "\\",
+    "^",
+    "_",
+    "`",
+    "|",
+    "~",
+    "¥",
+    "¦",
+    "°",
+    "±",
+  ];
+  p5.setup = () => {
+    const canvas = p5.createCanvas(1144, 1640);
+    canvas.style("display", "block");
+    canvas.style("outline", "none");
+    canvas.style("background", "transparent");
+    console.info(characters[counter]);
+    glyph1 = new DSEG14(p5, 0, 0, 0.1, null, null, characters[counter]);
+    glyph2 = new DSEG14(p5, 130, 0, 0.1, null, null, "0");
+    setInterval(increment, 1000);
+    numerals.push(new DSEG14(p5, 0, 200, 0.1, null, null, characters[counter]));
+    numerals.push(new DSEG14(p5, 130, 200, 0.1, null, null, characters[counter]));
+    numerals.push(new DSEG14(p5, 260, 200, 0.1, null, null, characters[counter]));
+    numerals.push(new DSEG14(p5, 390, 200, 0.1, null, null, characters[counter]));
+    numerals.push(new DSEG14(p5, 520, 200, 0.1, null, null, characters[counter]));
+    numerals.push(new DSEG14(p5, 650, 200, 0.1, null, null, characters[counter]));
+  };
+
+  p5.draw = () => {
+    p5.background("rgba(0, 255, 0, 0)");
+    glyph1.draw();
+    glyph2.draw();
+
+    const tempNumbers = getTimeArray();
+    for (let i = 0; i < 6; i++) {
+      numerals[i].setState(tempNumbers[i]);
+      numerals[i].draw();
+    }
+  };
+});
+
