42c693a17898f1300e9dc718f81c068f75102209
Dominic Myers <annoyingmouse@gmail.com>
2025-02-20 15:59:50 +0000

    ✨ (DotCircle.js): Introduce DotCircle class to handle drawing rotating dots in a circle 📝 (index.html): Add HTML structure and script tag to include sketch.js for DotCircle animation 📝 (sketch.js): Create p5 sketch to animate DotCircle instances with varying numbers of dots 📝 (README.md): Update README with a new project entry for DOT_ROTATE, mentioning the fun and math involved

---
+++ 000-MISCELLANEOUS/DOT_ROTATE/DotCircle.js
@@ -0,0 +1,33 @@
+export class DotCircle {
+  constructor(p5, fill, x, y, dotWidth, number, speed) {
+    this.p5 = p5;
+    this.fill = fill;
+    this.x = x;
+    this.y = y;
+    this.dotWidth = dotWidth;
+    this.number = number;
+    this.circumference = this.number * (this.dotWidth * 2);
+    this.diameter = this.circumference / Math.PI;
+    this.step = p5.TWO_PI / this.number;
+    this.increment = 0;
+  }
+  draw() {
+    this.p5.fill(this.fill);
+    this.p5.noStroke();
+    for (
+      let j = 0 + this.increment;
+      j < this.p5.TWO_PI + this.increment;
+      j += this.step
+    ) {
+      this.p5.circle(
+        this.x + (Math.cos(j) * this.diameter) / 2,
+        this.y + (Math.sin(j) * this.diameter) / 2,
+        this.dotWidth,
+      );
+    }
+    this.increment += 360 / this.number / 3600;
+    if (this.increment >= 360) {
+      this.increment = 0;
+    }
+  }
+}
---
+++ 000-MISCELLANEOUS/DOT_ROTATE/sketch.js
@@ -0,0 +1,52 @@
+import { p5 } from "https://cdn.skypack.dev/p5js-wrapper";
+import { DotCircle } from "./DotCircle.js";
+
+new p5((p5) => {
+  const main = document.querySelector("main");
+  const verticalHeight = main.clientWidth;
+  const backGroundColour = "#FF0000";
+  const dotWidth = 20;
+  let circumference = null;
+  let diameter = null;
+  let step = null;
+  let dotCircles = [];
+
+  p5.setup = () => {
+    p5.angleMode(p5.DEGREES);
+    p5.createCanvas(verticalHeight, verticalHeight);
+    p5.background(backGroundColour);
+    p5.fill(0);
+    p5.noStroke();
+    p5.circle(p5.width / 2, p5.height / 2, dotWidth);
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 6),
+    );
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 12),
+    );
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 18),
+    );
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 24),
+    );
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 30),
+    );
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 36),
+    );
+    dotCircles.push(
+      new DotCircle(p5, 0, p5.width / 2, p5.height / 2, dotWidth, 42),
+    );
+  };
+  p5.draw = () => {
+    p5.background(backGroundColour);
+    p5.fill(0);
+    p5.noStroke();
+    p5.circle(p5.width / 2, p5.height / 2, dotWidth);
+    dotCircles.forEach((dotCircle, index) => {
+      dotCircle.draw();
+    });
+  };
+});
