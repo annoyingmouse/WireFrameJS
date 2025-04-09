c8648b7d109757349dade397368e6e5221c74f92
Dominic Myers <annoyingmouse@gmail.com>
2025-04-09 13:52:58 +0100

    ✨ (ROTATING_BLOCKS03): Add new rotating block functionality with dynamic angle and velocity based on position ♻️ (ROTATING_BLOCKS04): Refactor rotating block functionality to decrease angle with a constant velocity ✨ (ROTATING_BLOCKS05): Add new rotating block functionality with increasing angle and velocity based on position

---
+++ 000-MISCELLANEOUS/ROTATING_BLOCKS04/RotatingBlock.js
@@ -0,0 +1,39 @@
+export class RotatingBlock {
+  constructor(
+    p5,
+    backgroundColour = "#000000",
+    fill = "#FFFFFF",
+    x,
+    y,
+    width,
+    angle = 0,
+    velocity = 0.01,
+  ) {
+    this.p5 = p5;
+    this.backgroundColour = backgroundColour;
+    this.fill = fill;
+    this.x = x - width / 4;
+    this.y = y - width / 4;
+    this.angle = angle;
+    this.width = width * 1.5;
+    this.height = width * 1.5;
+    this.velocity = velocity;
+  }
+  draw() {
+    this.p5.fill(this.backgroundColour);
+    this.p5.noStroke();
+    this.p5.rect(this.x, this.y, this.width, this.height);
+    this.p5.fill(this.fill);
+    this.p5.push();
+    this.p5.translate(this.x + this.height / 2, this.y + this.height / 2);
+    this.p5.rotate(this.angle);
+    this.p5.rect(
+      -(this.width / 6),
+      -(this.height / 2),
+      this.width / 3,
+      this.height,
+    );
+    this.p5.pop();
+    this.angle -= this.velocity;
+  }
+}
---
+++ 000-MISCELLANEOUS/ROTATING_BLOCKS04/sketch.js
@@ -0,0 +1,56 @@
+import { p5 } from "https://cdn.skypack.dev/p5js-wrapper";
+import { RotatingBlock } from "./RotatingBlock.js";
+const distance = (x1, y1, x2, y2) => {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+new p5((p5) => {
+  const main = document.querySelector("main");
+  const verticalHeight = main.clientWidth;
+  const backGroundColour = "#000000";
+  const blockWidth = Math.floor(verticalHeight / 20);
+  const blockHeight = blockWidth;
+  let blocks = [];
+  const distances = [];
+
+  p5.setup = () => {
+    p5.angleMode(p5.DEGREES);
+    p5.createCanvas(verticalHeight, verticalHeight);
+    p5.background(backGroundColour);
+    const halfBlock = blockWidth / 2;
+    const [centreX, centreY] = [verticalHeight / 2, verticalHeight / 2];
+    for (
+      let x = blockWidth, iteration_x = 0;
+      x <= verticalHeight - blockWidth * 2, iteration_x < 18;
+      x += blockWidth, iteration_x++
+    ) {
+      for (
+        let y = blockHeight, iteration_y = 0;
+        y <= verticalHeight - blockHeight * 2, iteration_y < 18;
+        y += blockHeight, iteration_y++
+      ) {
+        distances.push(distance(x + halfBlock, y + halfBlock, centreX, centreY));
+        blocks.push(
+          new RotatingBlock(
+            p5,
+            "#00000000",
+            "#FFFFFF",
+            x,
+            y,
+            blockWidth,
+            distance(x + halfBlock, y + halfBlock, centreX, centreY) / 5,
+            0.5,
+          ),
+        );
+      }
+    }
+  };
+  p5.draw = () => {
+    p5.background(backGroundColour);
+    blocks.forEach((block) => {
+      block.draw();
+    });
+  };
+});
